439b8798c8bc1da265d58de48f26dc2b96cd6a68
Parv Jain <parvj69@gmail.com>
2025-07-29 10:29:27 +0530

    Add

---
+++ module3 conditionalstatementss/NesteForLoopDemo.java
@@ -0,0 +1,23 @@
+public class NesteForLoopDemo {
+    public static void main(String[] args) {
+        int size = 5;
+        System.out.println("Star Pyramid:");
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nMultiplication Table:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
