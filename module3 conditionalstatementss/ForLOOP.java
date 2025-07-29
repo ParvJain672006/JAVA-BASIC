797ae020ff0a90d63345b991c5a75560ae7fb5aa
Parv Jain <parvj69@gmail.com>
2025-07-29 10:26:16 +0530

    add

--- module3 conditionalstatementss/ForLOOP.java
+++ module3 conditionalstatementss/ForLOOP.java
@@ -1,10 +1,19 @@
-
-
 public class ForLOOP {
     public static void main(String[] args) {
-    for(int i = 0 ; i <=5 ; i++){
-        System.out.println("i = " + i);
+        String[] animals = {"Lion", "Tiger", "Elephant", "Giraffe", "Zebra", "Monkey"};
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println("Animal #" + (i + 1) + ": " + animals[i]);
+        }
 
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of numbers from 1 to 10 is: " + sum);
+
+        for (int i = 5; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
-}
 };
